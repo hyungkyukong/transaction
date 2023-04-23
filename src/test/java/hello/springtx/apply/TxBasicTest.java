b88a797b53c056efdfe328e808e46e07c261e57e
hkkong <konguuu@gmail.com>
2023-04-23 21:04:25 +0900

    first

---
+++ src/test/java/hello/springtx/apply/TxBasicTest.java
@@ -0,0 +1,58 @@
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+@SpringBootTest
+public class TxBasicTest {
+
+    @Autowired BasicService basicService;
+
+    @Test
+    void proxyCheck() {
+        log.info("aop class={}", basicService.getClass());
+        Assertions.assertThat(AopUtils.isAopProxy(basicService)).isTrue();
+    }
+
+    @Test
+    void txTest() {
+        basicService.tx();
+        basicService.nonTx();
+    }
+
+    @TestConfiguration
+    static class TxApplyBasicConfig {
+        @Bean
+        BasicService basicService() {
+            return new BasicService();
+        }
+    }
+
+
+    @Slf4j
+    static class BasicService {
+        @Transactional
+        public void tx() {
+            log.info("call tx");
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            //여기에 돌고있는 스레드에서 트랜잭연이 적용되는게 맞냐는거를 알수가있다고 한다.
+            log.info("tx active={}", txActive);
+        }
+
+        public void nonTx() {
+            log.info("call nonTx");
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            //여기에 돌고있는 스레드에서 트랜잭연이 적용되는게 맞냐는거를 알수가있다고 한다.
+            log.info("nonTx active={}", txActive);
+        }
+    }
+}
